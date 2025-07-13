f918d46f03a41b3c8bbd02a4f754977115d19f79
Dustin Carlino <dabreegster@gmail.com>
2025-07-13 16:25:15 +0200

    Find smallest matching overpass region

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import booleanContains from "@turf/boolean-contains";
+import area from "@turf/area";
 import { execSync } from "child_process";
 import type { MultiPolygon, FeatureCollection } from "geojson";
 
@@ -11,28 +13,45 @@ async function main() {
 }
 
 async function findSmallest(gjPath: string) {
+  let boundary = JSON.parse(fs.readFileSync(gjPath, { encoding: "utf8" }));
+  if (
+    boundary.type != "Feature" ||
+    !["Polygon", "MultiPolygon"].includes(boundary.geometry.type)
+  ) {
+    throw new Error(
+      `${gjPath} isn't a GJ file with one Polygon or MultiPolygon boundary`,
+    );
+  }
+
   let overpassIndex = getOverpassIndex();
-  console.log(overpassIndex);
+  for (let f of overpassIndex.features) {
+    if (booleanContains(f, boundary)) {
+      console.log(f.properties);
+      return;
+    }
+  }
+  throw new Error(`No Overpass region contains the boundary from ${gjPath}`);
 }
 
 function getOverpassIndex(): FeatureCollection<MultiPolygon, {}> {
   try {
-    let file = fs.readFileSync("overpass_cache/index-v1.json", {
-      encoding: "utf8",
-    });
-    return JSON.parse(file);
+    fs.readFileSync("overpass_cache/index-v1.json");
   } catch (err) {
     console.log("Overpass index missing, downloading it");
     execSync("mkdir -p overpass_cache");
     execSync(
       "wget https://download.geofabrik.de/index-v1.json -O overpass_cache/index-v1.json",
     );
-
-    let file = fs.readFileSync("overpass_cache/index-v1.json", {
-      encoding: "utf8",
-    });
-    return JSON.parse(file);
   }
+
+  let gj = JSON.parse(
+    fs.readFileSync("overpass_cache/index-v1.json", {
+      encoding: "utf8",
+    }),
+  );
+  // Sort by smallest area first
+  gj.features.sort((a: any, b: any) => area(a) - area(b));
+  return gj;
 }
 
 main();
